b6fa6abbe991c1507b1806a6a6bfc357e8401e49
yohhei someya <youhei19880130@yahoo.co.jp>
2017-07-06 11:34:00 +0900

    Add basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  http_basic_authenticate_with name: 'styles', password: 'bank' if Rails.env == 'production'
 end

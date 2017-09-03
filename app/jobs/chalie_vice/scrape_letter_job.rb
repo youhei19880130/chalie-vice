0051bb03ae5df3059dcb43758227a3b9c9c85e24
yohhei someya <youhei19880130@yahoo.co.jp>
2017-09-04 01:05:44 +0900

    Temporaly modify ChalieVice::ScrapeLetterJob

--- app/jobs/chalie_vice/scrape_letter_job.rb
+++ app/jobs/chalie_vice/scrape_letter_job.rb
@@ -6,24 +6,24 @@ module ChalieVice
     def perform(url)
       doc = open_nokogiri_document(url)
 
-      title = xpath_text(doc, '/html/body/div[2]/section/div/div[1]/h1')
-      logger.info("title: #{title}")
-      description = doc.xpath('/html/body/div[2]/section/div/div[1]/*[2]')
-                       .to_s.delete("\n").gsub(/^<p>/, '').gsub(%r{<\/p>}, '')
-      category = xpath_text(doc, '/html/body/div[2]/ul/li[3]/a/span')
-      logger.info("category: #{category}")
+      #title = xpath_text(doc, '/html/body/div[2]/section/div/div[1]/h1')
+      #logger.info("title: #{title}")
+      #description = doc.xpath('/html/body/div[2]/section/div/div[1]/*[2]')
+      #                 .to_s.delete("\n").gsub(/^<p>/, '').gsub(%r{<\/p>}, '')
+      #category = xpath_text(doc, '/html/body/div[2]/ul/li[3]/a/span')
+      #logger.info("category: #{category}")
       thumb_image = doc.xpath('//div[2]/section/section/p/img').attr('src')
       logger.info("thumb_image: #{thumb_image}")
       letter = ChalieVice::Letter.where(url: url).first
       letter.remote_thumb_image_url = thumb_image
       letter.save
-      doc.xpath('//div[2]/section/div/div[1]//p/img').map do |img|
-        src = img.attr('src')
-        logger.info("image_url: #{src}")
-        # TODO: 写真を保存する処理
-        description.gsub!(src, 'hogehoge')
-      end
-      logger.info("description: #{description}")
+      #doc.xpath('//div[2]/section/div/div[1]//p/img').map do |img|
+      #  src = img.attr('src')
+      #  logger.info("image_url: #{src}")
+      #  # TODO: 写真を保存する処理
+      #  description.gsub!(src, 'hogehoge')
+      #end
+      #logger.info("description: #{description}")
     end
 
     private
